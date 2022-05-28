0fd1c7cd900af7dc466afd4d59ced3f29e354e83
Sergio Compean <scompean24@gmail.com>
2022-05-28 02:04:01 -0400

    ignore cache clear

--- src/renderer/vendor/surgiie/blade-cli/src/Support/Exceptions/FileNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BladeCLI\Support\Exceptions;
-
-use ErrorException;
-
-class FileNotFoundException extends ErrorException
-{
-}
--- src/renderer/vendor/surgiie/blade-cli/src/Support/Exceptions/UndefinedVariableException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BladeCLI\Support\Exceptions;
-
-use ErrorException;
-
-class UndefinedVariableException extends ErrorException
-{
-}
